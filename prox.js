37bc80022946590fbfa32e6a05546da53cad21e0
huberlv <357370736@qq.com>
2014-06-10 23:14:22 +0800

    基本版
    基本版

--- prox.js
+++ prox.js
@@ -1,18 +1,14 @@
 var net = require('net');
+var dns = require('dns');
 var timeout = 20000;//超时
-var listenPort = 800;//监听端口
+var listenPort = 80;//监听端口
 
 var server = net.createServer(function(socket){
     // 我们获得一个连接 - 该连接自动关联一个socket对象
     console.log('connect: ' +
         socket.remoteAddress + ':' + socket.remotePort+", "+socket["host"]);
-    for(var i in socket){
-		if( typeof(socket[i])== "object"){
-			 console.log(i+":---:"+socket[i]);
-		}
-	}
 
-    socket.setEncoding('binary');
+    //socket.setEncoding('binary');
 
     //超时事件
 //    socket.setTimeout(timeout,function(){
@@ -21,9 +17,41 @@ var server = net.createServer(function(socket){
 //    });
 
     //接收到数据
-    socket.on('data',function(data){
+    socket.on('data',function(heads){
 
-        console.log('recv:' + data);
+        console.log('recv:' + heads);
+		var head=getHeads(heads.toString());
+
+		var REMOTE_ADDR=head['Host'];
+ console.log('REMOTE_ADDR:' + REMOTE_ADDR);
+ //REMOTE_ADDR="www.baidu.com";
+
+       var port=80;
+		if(REMOTE_ADDR.charAt(0)=='1' ||REMOTE_ADDR.charAt(0)=='2'){
+			var index=REMOTE_ADDR.indexOf(':');
+			if(index>-1){
+				port=parseInt(REMOTE_ADDR.substring(index+1,REMOTE_ADDR.length));
+				REMOTE_ADDR=REMOTE_ADDR.substring(0,index);
+			}
+		}
+        dns.lookup(REMOTE_ADDR,null,function(err,addr,family){
+		 
+			console.log('addr:' + addr);
+			var serviceSocket = new net.Socket();
+			// serviceSocket.setEncoding('binary');
+			 console.log('port:' + port);	 console.log('addr:' + addr);
+			serviceSocket.connect(port, addr, function () {
+				console.log('>> From proxy to remote',heads);
+				serviceSocket.write(heads);
+			});
+			serviceSocket.on("data", function (data) {
+				console.log('<< From remote to proxy',data);
+				socket.write(data);
+				console.log('>> From proxy to client');
+			});
+		}
+		);
+		
 
     });
 
@@ -38,20 +66,6 @@ var server = net.createServer(function(socket){
             socket.remoteAddress + ' ' + socket.remotePort);
 
     });
-
-
-
-		var REMOTE_ADDR="www.baidu.com";
-        var serviceSocket = new net.Socket();
-        serviceSocket.connect(80, REMOTE_ADDR, function (msg) {
-            console.log('>> From proxy to remote');
-            serviceSocket.write(msg);
-        });
-        serviceSocket.on("data", function (data) {
-            console.log('<< From remote to proxy');
-            socket.write(data);
-            console.log('>> From proxy to client');
-        });
 	//socket.end();
 }).listen(listenPort);
 
@@ -64,3 +78,21 @@ server.on('listening',function(){
 server.on("error",function(exception){
     console.log("server error:" + exception);
 }); 
+
+function getHeads(head){
+	var result=new Object();
+	var index=0;
+	for (var line in head.split('\n') ){
+		 index=line.indexof();
+         result
+	}
+}
+
+function getHeads(head){
+	var h="Host:";
+	var result=new Object();
+	var index=0;
+	index=head.indexOf(h,0);
+    result["Host"]=head.substring(index+h.length,head.indexOf("\n",index)).replace(" ","");
+	return result;
+}
